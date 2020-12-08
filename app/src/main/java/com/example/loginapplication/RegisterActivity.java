e1dc938a19b24aa87b1d2493afc09a4fbddb5ece
Anton van Someren <acrbvansomeren@gmail.com>
2020-12-08 23:40:16 +0100

    temp bestanden na MEGA crash

--- app/src/main/java/com/example/loginapplication/RegisterActivity.java
+++ app/src/main/java/com/example/loginapplication/RegisterActivity.java
@@ -1,13 +1,63 @@
 package com.example.loginapplication;
 
+import android.content.Intent;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class RegisterActivity extends AppCompatActivity {
+    EditText eFullName, eEmail, ePassword;
+    Button ebtnRegister;
+    TextView ebtnLogin;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        eFullName = findViewById(R.id.edtFullName);
+        eEmail = findViewById(R.id.edtEmail);
+        ePassword = findViewById(R.id.edtPassword);
+        ebtnRegister = findViewById(R.id.btnRegister);
+        ebtnLogin = findViewById(R.id.tvAlreadyAnAccount);
+
+
+
+        ebtnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = eEmail.getText().toString().trim();
+                String password = ePassword.getText().toString().trim();
+
+                if(TextUtils.isEmpty(email)){
+                    eEmail.setError("Email is verplicht");
+                    return;
+                }
+                if(TextUtils.isEmpty(password)){
+                    ePassword.setError("Wachtwoord is verplicht");
+                    return;
+                }
+
+                if(password.length() < 6){
+                    ePassword.setError("Wachtwoord moet langer dan 6 karakters zijn");
+                    return;
+                }
+
+
+            }
+        });
+
+        ebtnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(RegisterActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
